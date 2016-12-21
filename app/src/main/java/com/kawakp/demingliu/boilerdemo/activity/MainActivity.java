953d145095ae4e9650d8b7498bcb71c64c5f41b2
deming.liu <ldming2015@163.com>
2016-12-21 14:10:43 +0800

    Initial commit

---
+++ app/src/main/java/com/kawakp/demingliu/boilerdemo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kawakp.demingliu.boilerdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kawakp.demingliu.boilerdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
